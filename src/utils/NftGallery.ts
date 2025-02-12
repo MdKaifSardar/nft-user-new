37f5154ebae58542f629f7662aaf06e4666e3268
Md Kaif Sardar <mkaifsard564773@gmail.com>
2025-02-12 23:14:24 +0530

    added new images

--- src/utils/NftGallery.ts
+++ src/utils/NftGallery.ts
@@ -1,10 +1,13 @@
-import image1 from "../assets/images/image1.jpg";
+import image1 from "../assets/images/nfts/asset 1.png";
+import image2 from "../assets/images/nfts/asset 2.png";
+import image3 from "../assets/images/nfts/asset 3.png";
+import image4 from "../assets/images/nfts/asset 4.png";
 
 const nfts = [
   { id: "704451", image: image1, price: "294 USDT" },
-  { id: "610665", image: image1, price: "294 USDT" },
-  { id: "266723", image: image1, price: "294 USDT" },
-  { id: "335895", image: image1, price: "294 USDT" },
+  { id: "610665", image: image2, price: "294 USDT" },
+  { id: "266723", image: image3, price: "294 USDT" },
+  { id: "335895", image: image4, price: "294 USDT" },
 ];
 
 export { nfts };
